5d78e733e4f797175e824b36d0c06add6eb7fd43
chandlerzx <3490635038@qq.com>
2019-01-09 21:55:11 +0800

    feat:购物车

--- src/js/detail.js
+++ src/js/detail.js
@@ -1,7 +1,7 @@
 require(["./requirejs.config"], () => {
   require(["jquery", "url","template", "header","footer","cookie"], ($, url,template) => {
-  		
   	//连接口
+  	console.log("8885");
     $(function(){
       //获取id
       let arrSearch = location.search.slice(1).split("=");
@@ -21,48 +21,66 @@ require(["./requirejs.config"], () => {
           $(".detail-bag-img").attr("src",res.res_body.img);
           $("#price").html("￥"+res.res_body.price);
           $("#title").html(res.res_body.title);
-         	
-         	
+  	
          		//存cookie
          		  //添加到购物车事件
+  		 
 		    $(".addshopcar").on("click",function(){
-		    	    	
-		    		var title = res.res_body.title,
+		    	
+		    	 var title = res.res_body.title,
 								price = res.res_body.price,
 								img = res.res_body.img;
-						var obj ={
-						"title":title,
-						"price":price,
-						"img":img
-					};
-					console.log(obj);
-					$.cookie("car",JSON.stringify(obj));
-		    	
-					console.log($.cookie("car"));
+						var obj = {
+								id:searchObj.id,
+								title:title,
+								price:price,
+								img:img,
+								num:1
+							};
+							var flag = true;
+					//console.log(obj);				
+			var arr = $.cookie("car")? JSON.parse($.cookie("car")) : [] ;
+						console.log(obj);
+						console.log(arr);
+						
+		
 					
+		for(let i = 0 ;i<arr.length;i++){
+				
+					
+					if(arr[i].id == obj.id){					
+							console.log(arr[i].num);
+							arr[i].num++;
+							flag=false;
+					}
+					if(flag === true){
+							arr.push(obj); 
+					}				
+				}		
+
+					arr.push(obj); 
+					$.cookie("car",JSON.stringify(arr));
+					console.log($.cookie("car"));
+
+		
+			
+			$.cookie("car",JSON.stringify(arr));
+			
+			console.log($.cookie("car"));
+			
+				if(confirm("添加购物袋完成,是否去查看我的购物袋？")){
 		    	window.location.href = "/html/shopcar.html";
-		    });
+		   };
    				 //添加购物车完成
          	//存cookie
-
          	
-        }
-						
-      })
-	//连接口完成
+      		 })//加购物车
+					}//sucess
+      })//连接口完成
 	
-	
-						
-	
+
     })
-    
-    
-    
-  
-    
- 
-    
-    
     
   })
 })
+  
--- src/js/shopcar.js
+++ src/js/shopcar.js
@@ -2,26 +2,151 @@
 require(["./requirejs.config"], () => {
 	//引入index需要依赖的模块
 require(["jquery","header","footer","cookie"], ($) => {
-	
-	
-	var arr = JSON.parse($.cookie("car"));
-	console.log(arr);
-	var bagnum = 1;
+
+	//拼接
+	var carobj = JSON.parse($.cookie("car"));
+	var arr = [];
 	var str = "";
-//	for(var value of arr){	
-		str += '<tr>'+
-	            '<td>'+ arr.title +'</td>'+
-	            '<td class="zs"><a href="javascript:;">+</a href="javascript:;"><span>'+bagnum+'</span><a href="javascript:;">-</a href="javascript:;"></td>'+
-	            '<td><span>'+ arr.price +'</span><input type="text"></td>'+
-	            '<td><span></span></td>'+
-	      	 	'</tr>';
-	      	 
-//		}
+	var allmoney = 1;
+	
+	
+//拼接
+	for(var key in carobj){
+		var lasttd =carobj[key].num*carobj[key].price;
+		str += '<tr class="shop-tr">'+
+	            '<td>'+ carobj[key].title +'</td>'+
+	            '<td class="zs"><a href="javascript:;" class="zs-num addbtn">+</a><span class="bagnum">'+carobj[key].num+
+	            '</span><a href="javascript:;" class="zs-num lessbtn">-</a></br><a href="javascript:;" class="delBtn">移除</a></td>'+
+	            '<td class="shop-price"><span class="inner-price">￥'+ carobj[key].price +'</span></td>'+
+	            '<td class="single-add"><span class="allmoney">'+lasttd+'</span></td>'+
+	      	 	'</tr>';   	 
+		}
 
 				$(".content-tbody").html(str);
-		
-		
+				
+				
+				
+	for (let i in carobj) {
+   
+  	 arr.push(carobj[i]);
+    //遍历对象中的num
+	}			
+    //增加数量按钮  		
+	//加减按钮绑定事件
 	
+//	$(".lessbtn").on("click",function(){
+//		$(".bagnum").html(--bagnum);
+//		if(bagnum<=1){
+//		$(".bagnum").html(1);
+//		}
+//		add();
+//	})
+		
+	//事件委派来增加减少
+	//增加
+	$(".shop-tr").on("click",".addbtn",function(){
+			var detailnum = $(this).siblings(".bagnum").html();
+			$(this).siblings(".bagnum").html(++detailnum);
+			var dj = $(this).parent().siblings(".shop-price").children(".inner-price").html().slice(1);
+			var sl = $(this).siblings(".bagnum").html();
+			
+			var singlesum = dj*sl;
+			$(this).parent().siblings(".single-add").children(".allmoney").html(singlesum);
+			
+			//取下标
+			var index = $(this).parent().parent().index();
+//			console.log(index);
+//			console.log(arr[index].num);
+			arr[index].num++;
+			//取小计
+			let sumall = 0;
+			for(var key of carobj){
+				
+				sumall += key.num*key.price;
+				
+			}
+			console.log(sumall);
+			$("#money").html(sumall);
+			$.cookie("car",JSON.stringify(arr));
+			console.log(arr);
+		
+	});
+	//减法按钮
+	$(".shop-tr").on("click",".lessbtn",function(){
+			var detailnum = $(this).siblings(".bagnum").html();
+			console.log(detailnum);
+			$(this).siblings(".bagnum").html(--detailnum);
+			if(detailnum <= 1){
+				$(this).siblings(".bagnum").html(1);
+			}
+			var dj = $(this).parent().siblings(".shop-price").children(".inner-price").html().slice(1);
+			var sl = $(this).siblings(".bagnum").html();
+			
+			var singlesum = dj*sl;
+			$(this).parent().siblings(".single-add").children(".allmoney").html(singlesum);
+			
+				console.log(singlesum);
+			//cookie
+			var index = $(this).parent().parent().index();
+//			console.log(index);
+//			console.log(arr[index].num);
+			arr[index].num--;
+			//取小计
+			let sumall = 0;
+			for(var key of carobj){
+				
+				sumall += key.num*key.price;
+				
+			}
+			console.log(sumall);
+			$("#money").html(sumall);
+			$.cookie("car",JSON.stringify(arr));
+				console.log(arr);
+				
+	
+	});
+	
+	//总价结算
+	
+			shopcarall();
+		function shopcarall(){
+			var carobj = JSON.parse($.cookie("car"));
+			let sumall = 0;
+			for(var key of carobj){		
+				sumall += key.num*key.price;		
+			}
+			console.log(sumall);
+			$("#money").html(sumall);
+			$.cookie("car",JSON.stringify(arr));
+			console.log(arr);
+		}
+		
 
+
+
+	//移除事件
+		var otable = $(".table")[0];
+		otable.onclick = function(e){
+			
+			var target = e.target || e.srcElement;
+			var  tr = target.parentNode.parentNode;
+						
+			if(target.className==="delBtn"){//删除
+		//	var tr = target.parentNode.parentNode;
+			tr.parentNode.removeChild(tr);
+			$.cookie("car", "", {expires:-1});
+			console.log($.cookie("car"));
+		}
+			
+		}
+
+	//点击标题跳转回详情页
+	var jumpDetail = $("tr td")[0];
+	$(jumpDetail).on("click",function(){
+		window.location.href = "/html/detail.html";
+
+	})
+	
+//over
 	})
 })
